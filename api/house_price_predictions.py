0c7b7a922b213ff79ec0f6bed05caf836665fa31
Paulo Roberto <37346434+paulinhok14@users.noreply.github.com>
2025-08-03 14:56:13 -0300

    Adding base model with pydantic

--- api/house_price_predictions.py
+++ api/house_price_predictions.py
@@ -1,11 +1,25 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
+import os
 
+# Path names
+file_path = os.path.abspath(__file__)
+print(file_path)
 
 # Instancing API app
 app = FastAPI()
 
+# Input Features
+class InputFeatures(BaseModel):
+    pass
 
+
+# Adding complementary demographic data
+def add_complementary_data():
+    pass
+
+# Prediction endpoint
 @app.post('/predict')
 async def predict_house_price():
     pass
+
